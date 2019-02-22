de569aad41ee068877ca853a1bfe3336c151fcf0
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2019-02-22 20:54:45 +0300

    Meeeh...

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,53 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+      this.root = null;
+      this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+      this.insertNode(new Node(data, priority));
+      this.shiftNodeUp(new Node(data, priority));
 	}
 
 	pop() {
-		
+
 	}
 
 	detachRoot() {
-		
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+      return this.parentNodes.length;
 	}
 
 	isEmpty() {
-		
+      return this.parentNodes.length === 0;
 	}
 
 	clear() {
-		
+      this.root = null;
+      this.parentNodes = [];
 	}
 
-	insertNode(node) {
-		
+   insertNode(node) {
+      if (this.root === null) {
+         this.root = this.parentNodes.push(node);
+      }
+      this.parentNodes.push(node);
 	}
 
 	shiftNodeUp(node) {
-		
+      this.parentNodes.push(node);
 	}
 
 	shiftNodeDown(node) {
-		
+
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,22 +1,67 @@
 class Node {
 	constructor(data, priority) {
-
+      this.data = data;
+      this.priority = priority;
+      this.left = null;
+      this.right = null;
+      this.parent = null;
 	}
 
-	appendChild(node) {
+   appendChild(node) {
+      if (this.left === null) {
+         this.left = node;
+         this.left.parent = this;
 
+         return;
+      }
+
+      if (this.left && this.right === null) {
+         this.right = node;
+         this.right.parent = this;
+
+         return;
+      }
 	}
 
 	removeChild(node) {
+      if (this.right === node) {
+         this.right = null;
+         node.parent = null;
 
+         return;
+      }
+
+      if (this.left === node) {
+         this.left = null;
+         node.parent = null;
+
+         return;
+      }
+
+      if (this.right !== node && this.left !== node) {
+         throw new Error();
+      }
 	}
 
-	remove() {
+   remove() {
+      if (this.parent === null) {
+         return;
+      }
 
+      this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+      const child = this;
+      const parent = this.parent;
+
+      parent.left = child.left;
+      parent.right = child.right;
+      parent.parent = child.parent;
+
+      this.parent = child;
+
+      return;
 	}
 }
 
