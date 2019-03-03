991f3821f21493ade7aff653d63ed1a8a7a07dbb
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2019-03-04 01:39:44 +0300

    try

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,55 +1,71 @@
-const Node = require('./node');
+const Node = require("./node");
 
 class MaxHeap {
-	constructor() {
+   constructor() {
       this.root = null;
       this.parentNodes = [];
-	}
+   }
 
-	push(data, priority) {
-      this.insertNode(new Node(data, priority));
-      this.shiftNodeUp(new Node(data, priority));
-	}
+   push(data, priority) {
+      const node = new Node(data, priority);
 
-	pop() {
+      this.insertNode(node);
+      this.shiftNodeUp(node);
+   }
 
-	}
+   pop() {}
 
-	detachRoot() {
+   detachRoot() {
+      const detachedRoot = this.root;
+      const rootIndex = this.parentNodes.findIndex(el => el === detachedRoot);
 
-	}
+      if (rootIndex !== -1) {
+         this.parentNodes.splice(rootIndex, 1);
+      }
 
-	restoreRootFromLastInsertedNode(detached) {
+      this.root = null;
 
-	}
+      return detachedRoot;
+   }
 
-	size() {
+   restoreRootFromLastInsertedNode(detached) {}
+
+   size() {
       return this.parentNodes.length;
-	}
+   }
 
-	isEmpty() {
-      return this.parentNodes.length === 0;
-	}
+   isEmpty() {
+      return this.size() === 0;
+   }
 
-	clear() {
+   clear() {
       this.root = null;
       this.parentNodes = [];
-	}
+   }
 
    insertNode(node) {
-      if (this.root === null) {
-         this.root = this.parentNodes.push(node);
+      if (this.isEmpty()) {
+         this.root = node;
       }
+
+      // if (this.root === null) {
+      //    this.root = node;
+      // } else {
+      //    this.parentNodes[0].appendChild(node);
+
+      //    if (this.parentNodes[0].left && this.parentNodes[0].right) {
+      //       this.parentNodes.shift();
+      //    }
+      // }
+
       this.parentNodes.push(node);
-	}
+   }
 
-	shiftNodeUp(node) {
+   shiftNodeUp(node) {
       this.parentNodes.push(node);
-	}
+   }
 
-	shiftNodeDown(node) {
-
-	}
+   shiftNodeDown(node) {}
 }
 
 module.exports = MaxHeap;
